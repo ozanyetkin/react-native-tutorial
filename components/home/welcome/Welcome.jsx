e8c4d9205c2035f20aff298548d07f8270a35048
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2023-10-11 13:45:08 +0300

    popular jobs section finished, nearby jobs tbd

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
-    View,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    Image,
-    FlatList
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
 } from 'react-native';
 
 import { useRouter } from 'expo-router';
@@ -16,56 +16,56 @@ import { icons, SIZES } from '../../../constants';
 const jobTypes = ["Full Time", "Part Time", "Internship", "Freelance"]
 
 const Welcome = () => {
-    const router = useRouter();
-    const [activeJobType, setActiveJobType] = useState("Full Time");
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full Time");
 
-    return (
-        <View>
-            <View style={styles.container}>
-                <Text style={styles.userName}>Hello Yetkin!</Text>
-                <Text style={styles.welcomeMessage}>Find your perfect job</Text>
-            </View>
-            <View style={styles.searchContainer}>
-                <View style={styles.searchWrapper}>
-                    <TextInput
-                        style={styles.searchInput}
-                        value=""
-                        onChange={() => { }}
-                        placeholder="What are you looking for?"
-                    />
-                </View>
-
-                <TouchableOpacity>
-                    <Image
-                        source={icons.search}
-                        resizeMode='contain'
-                        style={styles.searchBtnImage}
-                    />
-                </TouchableOpacity>
-            </View>
-
-            <View style={styles.tabsContainer}>
-                <FlatList
-                    data={jobTypes}
-                    renderItem={({ item }) => (
-                        <TouchableOpacity
-                            style={styles.tab(activeJobType, item)}
-                            onPress={() => {
-                                setActiveJobType(item);
-                                router.push(`/search/${item}`)
-                            }}
-                        >
-                            <Text>{item}</Text>
-                        </TouchableOpacity>
-                    )}
-                    keyExtractor={item => item}
-                    contentContainerStyle={{ columnGap: SIZES.small }}
-                    horizontal
-                />
-            </View>
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Yetkin!</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => { }}
+            placeholder="What are you looking for?"
+          />
         </View>
 
-    )
+        <TouchableOpacity>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+    </View>
+
+  )
 }
 
 export default Welcome
