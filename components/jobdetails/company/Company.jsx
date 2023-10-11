998676dfe4427bd2bcdcbeafab5fda26937e5187
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2023-10-11 16:23:42 +0300

    wrapped the project up with job detail page and footer

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -2,11 +2,35 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 import styles from './company.style'
+import { icons } from '../../../constants'
+import { checkImageURL } from '../../../utils'
 
-const Company = () => {
+
+const Company = ({ companyLogo, jobTitle, companyName, Location }) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          source={{ uri: checkImageURL(companyLogo) ? companyLogo : 'https://via.placeholder.com/150' }}
+          style={styles.logoImage}
+        />
+      </View>
+
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} | </Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            style={styles.locationImage}
+            resizeMode="contain"
+          />
+        </View>
+      </View>
+
     </View>
   )
 }
